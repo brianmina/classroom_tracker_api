aa3c562081b75d75ae5df64820e97e33f41d6434
brian gutierrez <brian.mina17@gmail.com>
2023-08-10 05:22:57 -0400

    remove ssl verify

--- app/database.py
+++ app/database.py
@@ -10,7 +10,7 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
 SQLALCHEMY_DATABASE_URL = f'cockroachdb+psycopg2://{DB_USER}:{DB_PASS}@spark-clam-4749.g8z.cockroachlabs.cloud:26257/classroom_tracker' \
-                          f'?sslmode=verify-full'
+                          # f'?sslmode=verify-full&sslrootcert='$HOME'/.postgresql/root.crt'
 
 engine = create_engine(
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
